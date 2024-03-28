2dc1639793324dcb4409db0794377805fa56f4c2
csabagy00 <csaba.gyo00@gmail.com>
2024-03-28 13:09:36 +0100

    Added Sun class, created controller with a Get endpoint and added Services directory

---
+++ SolarWatch/SolarWatch/Controllers/SunController.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SolarWatch.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class SunController : ControllerBase
+{
+    public ILogger<SunController> _logger;
+
+    public SunController(ILogger<SunController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("Get")]
+    public ActionResult<Sun> SunGet([Required]string city)
+    {
+        _logger.Log(LogLevel.Information, "Get Request");
+        
+        
+        
+        return Ok();
+    }
+}
---
+++ SolarWatch/SolarWatch/Sun.cs
@@ -0,0 +1,12 @@
+namespace SolarWatch;
+
+public class Sun
+{
+    public DateTime Sunrise { get; set; }
+    
+    public DateTime Sunset { get; set; }
+    
+    public DateTime Date { get; set; }
+    
+    public string City { get; set; }
+}
