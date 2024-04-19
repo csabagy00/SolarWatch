4621587c33ed9abbdc2b2b0241c8cd470775a133
Csaba <csaba.gyo00@gmail.com>
2024-04-19 16:39:29 +0200

    Added LatLons to the Context class.

--- SolarWatch/SolarWatch/Data/SolarWatchContext.cs
+++ SolarWatch/SolarWatch/Data/SolarWatchContext.cs
@@ -6,11 +6,13 @@ public class SolarWatchContext : DbContext
 {
     public DbSet<City> Cities { get; set; }
     
-    public DbSet<SunriseSunset> SunriseSunsets { get; set; }
+    public DbSet<Sun> Suns { get; set; }
+    
+    public DbSet<LatLon> LatLons { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(
             "Server=localhost,1433;Database=WeatherApi;User Id=sa;Password=Admin12345;Encrypt=false;");
     }
-}
+}   
