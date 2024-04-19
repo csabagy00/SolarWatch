5514f93cfb39d3c2db071c6d7ddb23088847dad4
Csaba <csaba.gyo00@gmail.com>
2024-04-19 16:36:28 +0200

    Edited the Sun class to be in the database

--- SolarWatch/SolarWatch/Models/Sun.cs
+++ SolarWatch/SolarWatch/Models/Sun.cs
@@ -2,11 +2,9 @@ namespace SolarWatch;
 
 public class Sun
 {
+    public int Id { get; set; }
     public string Sunrise { get; set; }
-    
     public string Sunset { get; set; }
-    
     public string Date { get; set; }
-    
     public string City { get; set; }
 }
