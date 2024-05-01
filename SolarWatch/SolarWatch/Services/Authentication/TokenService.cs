720273da2e86b50310427fc2515cd1fb4f7c52c9
csabagy00 <csaba.gyo00@gmail.com>
2024-05-01 14:39:09 +0200

    Implemented TokenService

---
+++ SolarWatch/SolarWatch/Services/Authentication/TokenService.cs
@@ -0,0 +1,75 @@
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SolarWatch.Services.Authentication;
+
+public class TokenService : ITokenService
+{
+    private const int ExpirationMinutes = 30;
+    
+    public string CreateToken(IdentityUser user)
+    {
+        var expiration = DateTime.UtcNow.AddMinutes(ExpirationMinutes);
+        var token = CreateJwtToken(
+            CreateClaims(user), 
+            CreateSigningCredentials(), 
+            expiration
+        );
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(token);
+    }
+
+    private JwtSecurityToken CreateJwtToken(List<Claim> claims, SigningCredentials signingCredentials, DateTime expiration)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        return new JwtSecurityToken(
+            config["AppSettings:ValidIssuer"],
+            config["AppSettings:ValidAudience"],
+            claims,
+            expires: expiration,
+            signingCredentials: signingCredentials);
+    }
+
+    private List<Claim> CreateClaims(IdentityUser user)
+    {
+        try
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "TokenForTheApiWithAuth"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            return claims;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private SigningCredentials CreateSigningCredentials()
+    {
+        var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+        Console.WriteLine($"From TokenServ : {config["securitykey"]}");
+        
+        return new SigningCredentials(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(config["securitykey"])),
+            SecurityAlgorithms.HmacSha256);
+    }
+}
