4e8a9b7193c11e4b40f1f55e4cf2a72f969aa1df
csabagy00 <csaba.gyo00@gmail.com>
2024-05-01 15:37:34 +0200

    Added roles to TokenService

--- SolarWatch/SolarWatch/Services/Authentication/TokenService.cs
+++ SolarWatch/SolarWatch/Services/Authentication/TokenService.cs
@@ -11,11 +11,11 @@ public class TokenService : ITokenService
 {
     private const int ExpirationMinutes = 30;
     
-    public string CreateToken(IdentityUser user)
+    public string CreateToken(IdentityUser user, string role)
     {
         var expiration = DateTime.UtcNow.AddMinutes(ExpirationMinutes);
         var token = CreateJwtToken(
-            CreateClaims(user), 
+            CreateClaims(user, role), 
             CreateSigningCredentials(), 
             expiration
         );
@@ -38,7 +38,7 @@ public class TokenService : ITokenService
             signingCredentials: signingCredentials);
     }
 
-    private List<Claim> CreateClaims(IdentityUser user)
+    private List<Claim> CreateClaims(IdentityUser user, string? role)
     {
         try
         {
@@ -51,6 +51,9 @@ public class TokenService : ITokenService
                 new Claim(ClaimTypes.Name, user.UserName),
                 new Claim(ClaimTypes.Email, user.Email)
             };
+            
+            if(role != null)
+                claims.Add(new Claim(ClaimTypes.Role, role));
 
             return claims;
         }
