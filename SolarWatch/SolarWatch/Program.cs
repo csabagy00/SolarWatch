3a7433bd58d7c73e52f45ede9ac190093a3e460d
csabagy00 <csaba.gyo00@gmail.com>
2024-05-01 12:11:37 +0200

    Registered new service and added IdentityCore options.

--- SolarWatch/SolarWatch/Program.cs
+++ SolarWatch/SolarWatch/Program.cs
@@ -1,9 +1,11 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Configuration;
 using SolarWatch.Data;
 using SolarWatch.Services;
+using SolarWatch.Services.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +21,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
 builder.Services.AddTransient<ISunsetSunriseApi, SunsetSunriseApi>();
 builder.Services.AddSingleton<IGeocodingApi, Geocoding>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddDbContext<SolarWatchContext>();
 builder.Services.AddDbContext<UsersContext>();
@@ -46,6 +49,19 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 Encoding.UTF8.GetBytes(secretValue))
         };
     });
+
+builder.Services
+    .AddIdentityCore<IdentityUser>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.User.RequireUniqueEmail = true;
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+    })
+    .AddEntityFrameworkStores<UsersContext>();
 
 var app = builder.Build();
 
