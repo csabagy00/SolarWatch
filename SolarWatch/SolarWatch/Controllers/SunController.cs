c3d98f25ffd011db961ca2a9ed029d5bfabaeb75
csabagy00 <csaba.gyo00@gmail.com>
2024-05-01 16:49:10 +0200

    Added dbContext field, implemented Patch and Post endpoint.

--- SolarWatch/SolarWatch/Controllers/SunController.cs
+++ SolarWatch/SolarWatch/Controllers/SunController.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SolarWatch.Data;
@@ -15,13 +17,15 @@ public class SunController : ControllerBase
     private readonly IJsonProcessor _jsonProcessor;
     private readonly IGeocodingApi _geocodingApi;
     private readonly ISunsetSunriseApi _sunsetSunriseApi;
+    private readonly SolarWatchContext _dbContext;
 
-    public SunController(ILogger<SunController> logger, IJsonProcessor jsonProcessor, IGeocodingApi geocodingApi, ISunsetSunriseApi sunsetSunriseApi)
+    public SunController(ILogger<SunController> logger, IJsonProcessor jsonProcessor, IGeocodingApi geocodingApi, ISunsetSunriseApi sunsetSunriseApi, SolarWatchContext dbContext)
     {
         _logger = logger;
         _jsonProcessor = jsonProcessor;
         _geocodingApi = geocodingApi;
         _sunsetSunriseApi = sunsetSunriseApi;
+        _dbContext = dbContext;
     }
 
     [HttpGet]
@@ -29,9 +33,8 @@ public class SunController : ControllerBase
     public async Task<ActionResult<Sun>> SunGet([Required]string city, [Required]string date)
     {
         _logger.Log(LogLevel.Information, "Get Request");
-        await using var dbContext = new SolarWatchContext();
 
-        var selectedSun = dbContext.Suns.FirstOrDefault(s => s.City == city && s.Date == date);
+        var selectedSun = _dbContext.Suns.FirstOrDefault(s => s.City == city && s.Date == date);
 
         if (selectedSun != null)
         {
@@ -66,6 +69,43 @@ public class SunController : ControllerBase
             _logger.LogError(e, $"Error getting data \n{e.Message}");
             return StatusCode(400);
         }
-        
+    }
+
+    [HttpPatch("Patch"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult<Sun>> SunPatch([Required]Sun sun)
+    { 
+        try
+        {
+            _logger.Log(LogLevel.Information, "Patch Request");
+
+            _dbContext.Suns.Update(sun);
+            await _dbContext.SaveChangesAsync();
+            
+            return Ok(sun);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error occured while patching.");
+            throw;
+        }
+    }
+
+    [HttpPost("Post"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult<Sun>> SunPost([Required] Sun sun)
+    {
+        try
+        {
+            _logger.Log(LogLevel.Information, "Post Request");
+
+            _dbContext.Suns.Add(sun);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(sun);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
