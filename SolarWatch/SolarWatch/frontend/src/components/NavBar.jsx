fe00c7ab7d7a4766d163705fd7414c9e55830a19
csabagy00 <csaba.gyo00@gmail.com>
2024-05-02 21:01:23 +0200

    Added conditional rendering to the NavBar

--- SolarWatch/SolarWatch/frontend/src/components/NavBar.jsx
+++ SolarWatch/SolarWatch/frontend/src/components/NavBar.jsx
@@ -2,19 +2,29 @@ import { Link } from "react-router-dom"
 import "./NavBar.css"
 
 
-function NavBar({ current }){
+function NavBar({ current, setToken, token }){
   return(
     <nav className="navbar">
       <Link to='/'>
         <button className='main'>SolarWatch</button>
       </Link>
       <div className="buttons">
+      { token == "" ? 
+      <>
         <Link to="/login">
           <button className="logreg">Login</button>
         </Link>
         <Link to="/registration">
           <button className="logreg">Register</button>
         </Link>
+      </>
+      : 
+      <>
+        <Link to='/'>
+          <button className="logreg" onClick={() => setToken("")}>Logout</button>
+        </Link>
+      </>
+      }
       </div>
     </nav>
   )
