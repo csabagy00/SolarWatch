9fb212ec5679724f0f7fc618d9b6d1b12df06f79
csabagy00 <csaba.gyo00@gmail.com>
2024-05-02 21:05:19 +0200

    File created for Sun data, added notes for tomorrow.

---
+++ SolarWatch/SolarWatch/frontend/src/components/SunData.jsx
@@ -0,0 +1,14 @@
+
+
+function SunData(){
+
+  /* Need to debug for the email@email.com account on the Backend, error: index out of range */
+
+  /* Table comes here for the sun Data */
+return(
+  <>
+  </>
+  )
+}
+
+export default SunData
