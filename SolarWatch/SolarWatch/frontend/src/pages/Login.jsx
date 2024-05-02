1132e8d7e645f69d0db45bc964fd60b5487b74e7
csabagy00 <csaba.gyo00@gmail.com>
2024-05-02 19:27:57 +0200

    Implemented Login page.

---
+++ SolarWatch/SolarWatch/frontend/src/pages/Login.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import './css/Form.css'
+
+function Login({ setToken }){
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/api/Auth/Login', {
+        method: 'POST',
+        headers: {
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      })
+
+      if(response.ok){
+        setEmail("")
+        setPassword("")
+        console.log("Login successful");
+        const result = response.json()
+        console.log(result);
+      }
+      
+    } catch (error) {
+      console.error('Error submitting login:', error);
+    }
+  }
+
+  return(
+    <div className='formDiv'>
+      <form onSubmit={handleSubmit}>
+        <div className='regInput'>
+          <label htmlFor='email'>Email:</label>
+          <input type='text' id='email' value={email} onChange={(e) => setEmail(e.target.value)} required/>
+        </div>
+        <div className='regInput'>
+          <label htmlFor='password'>Password:</label>
+          <input type='password' id='password' value={password} onChange={(e) => setPassword(e.target.value)} required/>
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  )
+
+}
+
+export default Login
