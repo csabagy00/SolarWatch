bc9a32d94bf4ba9821089877166181408b63cbc5
csabagy00 <csaba.gyo00@gmail.com>
2024-05-02 21:02:05 +0200

    Implemented handleSubmit for the search form, added states.

--- SolarWatch/SolarWatch/frontend/src/pages/MainPage.jsx
+++ SolarWatch/SolarWatch/frontend/src/pages/MainPage.jsx
@@ -1,6 +1,36 @@
+import { useState } from 'react'
+import SearchForm from '../components/SearchForm'
+import SunData from '../components/SunData'
 import './css/MainPage.css'
 
 function MainPage({ token }){
+  const [city, setCity] = useState("")
+  const [date, setDate] = useState("")
+  const [data, setData] = useState(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setCity("")
+    setDate("")
+
+    try {
+      const response = await fetch(`/api/Sun/Get?city=${city}&date=${date}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      })
+
+      const result = await response.json()
+
+      setData(result)
+      console.log(result)
+
+    } catch (error) {
+      console.error('Error submitting GET request', error)
+    }
+  }
 
   return(
     <div className='maindiv'>
@@ -12,7 +42,8 @@ function MainPage({ token }){
       ) :
       (
         <div>
-
+          <SearchForm setCity={setCity} setDate={setDate} city={city} date={date} handleSubmit={handleSubmit}/>
+          <SunData />
         </div>
       )}
     </div>
