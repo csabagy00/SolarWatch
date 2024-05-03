d0d86771733653603faf0531aeb8836416bb29f3
csabagy00 <csaba.gyo00@gmail.com>
2024-05-03 12:43:27 +0200

    Implemented protected solar-watch page.

--- SolarWatch/SolarWatch/frontend/src/App.jsx
+++ SolarWatch/SolarWatch/frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import { useState } from 'react'
 import Registration from './pages/Registration'
 import NavBar from './components/NavBar'
 import './App.css'
 import Login from './pages/Login'
 import MainPage from './pages/MainPage'
+import NotLoggedIn from './pages/NotLoggedIn'
 
 function App() {
   const [token, setToken] = useState("")
@@ -14,8 +15,9 @@ function App() {
     <BrowserRouter>
       <NavBar token={token} setToken={setToken}/>
         <Routes>
-          <Route path='/' element={<MainPage token={token}/>}/>
-          <Route path='/login' element={<Login setToken={setToken}/>}/>
+          <Route path='/' element={<NotLoggedIn />}/>
+          <Route path='/solar-watch' element={token != "" ? <MainPage token={token}/> : <Navigate to="/login"/>}/>
+          <Route path='/login' element={<Login setToken={setToken} token={token}/>}/>
           <Route path='/registration' element={<Registration />}/>
         </Routes>
     </BrowserRouter>
