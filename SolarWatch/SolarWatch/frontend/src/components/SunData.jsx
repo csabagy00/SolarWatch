128d5416942e5fd7556fdb0e5e3e6eeca1c96a52
csabagy00 <csaba.gyo00@gmail.com>
2024-05-03 12:45:23 +0200

    Implemented SunData card.

--- SolarWatch/SolarWatch/frontend/src/components/SunData.jsx
+++ SolarWatch/SolarWatch/frontend/src/components/SunData.jsx
@@ -1,13 +1,30 @@
+import './css/SunData.css'
 
+function SunData({ data }){
 
-function SunData(){
-
-  /* Need to debug for the email@email.com account on the Backend, error: index out of range */
-
-  /* Table comes here for the sun Data */
 return(
-  <>
-  </>
+  <div className="card">
+    <div className="cardinfo">
+      <div className='infodiv'>
+        <label htmlFor="cityname">City:</label>
+        <p className="pinfo">{data.city}</p>
+      </div>
+      <div className='infodiv'>
+        <label htmlFor="dateof">Date:</label>
+        <p className="pinfo">{data.date}</p>
+      </div>
+    </div>
+    <div className='sundata'>
+      <div className="sun">
+        <img src="../src/assets/sunrise.png" alt="Image" width="150" height="150"/>
+        <p className="suntime">{data.sunrise}</p>
+      </div>
+      <div className="sun">
+        <img src="../src/assets/sunset.png" alt="Image" width="150" height="150"/>
+        <p className="suntime">{data.sunset}</p>
+      </div>
+    </div>
+  </div>
   )
 }
 
